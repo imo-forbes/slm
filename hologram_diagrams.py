6dc5ea411c5bc6033a88ff72cef8833030bc6c26
Imogen Forbes <imogen.forbes@outlook.com>
2022-02-16 17:47:23 +0000

    w.i.p new decomp function

--- hologram_diagrams.py
+++ hologram_diagrams.py
@@ -15,23 +15,26 @@ Y = [236,236,236,256,256,256,276,276,276]
 
 radial = [0,1,1,2,2,2,3,3,3,3,4,4,4,4,4]
 azimuthal = [0,1,-1,0,-2,2,-1,1,-3,3,0,2,-2,4,-4]
-amplitude = [ 0.000,0.016,0.016,-0.043,-0.044,-0.000,-0.011,-0.011,-0.008,0.008, 0.069,0.000,0.057 , 0.128 ,-0.000]
+amplitude = [ 4.34738453e-01,  2.97358849e-03, -1.03608071e-02, -7.53345542e-03,
+       -7.22903243e-04, -4.35512934e-03,  7.83893365e-03,  7.60637623e-03,
+       -2.99139295e-02,  2.93378198e-02,  1.02432726e-02,  4.35653943e-02,
+        4.86483014e-03,  4.20554341e-03,  1.23038112e-04]
 
 
 
-trap_hologram = aags(traps = ((X[0],Y[0]), (X[1],Y[1]),(X[2],Y[2]), (X[3],Y[3]), (X[4],Y[4]),(X[5],Y[5]), (X[6],Y[6]), (X[7],Y[7]), (X[8],Y[8])),iterations=30,
-         beam_waist=None,beam_center=(256,256),shape=(512,512))#,circ_aper_center=None,):
+# trap_hologram = aags(traps = ((X[0],Y[0]), (X[1],Y[1]),(X[2],Y[2]), (X[3],Y[3]), (X[4],Y[4]),(X[5],Y[5]), (X[6],Y[6]), (X[7],Y[7]), (X[8],Y[8])),iterations=30,
+#          beam_waist=None,beam_center=(256,256),shape=(512,512))#,circ_aper_center=None,):
 
-MEDIUM_SIZE = 14
-BIGGER_SIZE = 16
+# MEDIUM_SIZE = 14
+# BIGGER_SIZE = 16
 
-plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
-plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-fig = plt.figure(figsize=(9,8))
+# plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+# plt.rc('axes', titlesize=MEDIUM_SIZE)    # fontsize of the x and y labels
+# plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+# plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
+# plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+# plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+# fig = plt.figure(figsize=(9,8))
 
 # x_res_ax = fig.add_subplot(gs[-2, :-1])
 # y_res_ax = fig.add_subplot(gs[-1, :-1])
@@ -51,10 +54,10 @@ fig = plt.figure(figsize=(9,8))
 # y_res_ax.set_yticks([-5, 0,5])
 
 
-plt.pcolor(trap_hologram)
-plt.colorbar()
-plt.savefig('images_for_seminar/seminar_9_traps.png', transparent = True)
-plt.show()
+# plt.pcolor(trap_hologram)
+# plt.colorbar()
+# plt.savefig('images_for_seminar/seminar_9_traps.png', transparent = True)
+# plt.show()
 
 # print(trap_hologram)
 
@@ -64,10 +67,10 @@ plt.show()
 # print(trap_hologram)
 # print(repr(trap_hologram))
 
-# zernike_holo=0
-# for i in range(len(radial)):
-#     holo = zernike(radial = radial[i], azimuthal = azimuthal[i], amplitude= amplitude[i], radius = 5, shape=(5,5))
-#     zernike_holo = zernike_holo + holo
+zernike_holo=0
+for i in range(len(radial)):
+    holo = zernike(radial = radial[i], azimuthal = azimuthal[i], amplitude= amplitude[i], radius = None, shape=(512,512))
+    zernike_holo = zernike_holo + holo
     
 
 
@@ -79,9 +82,10 @@ plt.show()
 
 
 # plt.figure(figsize=(5,5))
-# plt.pcolor(zernike_holo%1, cmap='viridis')
-# cb = plt.colorbar(label='Normalized Phase Change / $\mathrm{\pi}$ rads')
-# print(zernike_holo%1)
+plt.pcolor(zernike_holo, cmap='viridis')
+cb = plt.colorbar(label='Normalized Phase Change / $\mathrm{\pi}$ rads')
+print(zernike_holo)
+plt.show()
 
 # # # for l in cb.ax.yaxis.get_ticklabels():
 # # #    l.set_family("Times New Roman")
--- zernike_decomposition.py
+++ zernike_decomposition.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy import O
 from holograms.zernike import zernike, R
 from holograms.gratings import hori_gradient, vert_gradient, hori
 from holograms.apertures import circ
@@ -8,60 +9,106 @@ from matplotlib import gridspec
 from holograms.mixing import mix
 import matplotlib.pyplot as plt
 from slm import SLM
-
-def zernike_decomposition(hologram, 
-                        test_zernike_radial,
-                        test_zernike_azimuthal,
-                        test_amplitude, 
-                        circ_aperture = True, 
-                        x0 = None , 
-                        y0 = None , 
-                        radius = None, 
-                        shape = (512, 512)):
+from scipy.optimize import minimize
 
 
-    #if x0, y0 and radius are None, define as x0,y0 as centre of hologram and radius as max radius that can fit on SLM. 
+#same as Zernike polynomial function in SLM code but removes the amplitude factor and the circular aperture
+def zernike_no_aperture_no_amp(radial=0,azimuthal=0,x0=None,y0=None,radius=None,shape=(512,512)):
+
+    if azimuthal > radial:
+        raise ValueError("azimuthal must be less than or equal to radial")
     if x0 is None:
         x0 = shape[0]/2
     if y0 is None:
         y0 = shape[1]/2
     if radius is None:
         radius = min([shape[0]-x0,shape[1]-y0,x0,y0])
-
-    #define grid and coords
     x = np.arange(shape[0])
     y = np.arange(shape[1])
     xx,yy = np.meshgrid(x,y)
     r = np.sqrt((xx-x0)**2+(yy-y0)**2)
     r /= radius
-    print('r = ' + str(r))
     phi = np.arctan2(yy-y0,xx-x0)
-    print('phi = ' + str(phi))
-
-    #remove circular aperture if there is one present 
-    #Zernike function automatically adds this, aags does not so set accordingly.
-
-    if circ_aperture == True:
-        hologram -= circ(hologram, x0, y0, radius)
-
-    #scale by amplitude
-    hologram /= test_amplitude
-
-    if test_zernike_azimuthal >= 0:
-
-        hologram /= (R(r,test_zernike_radial,test_zernike_azimuthal)*np.cos(test_zernike_azimuthal*phi))
-
+    if azimuthal >= 0:
+        phase = R(r,radial,azimuthal)*np.cos(azimuthal*phi)
     else:
-        hologram /= (R(r,test_zernike_radial,-test_zernike_azimuthal)*np.sin(-test_zernike_azimuthal*phi))
+        phase = R(r,radial,-azimuthal)*np.sin(-azimuthal*phi)
+
+    return phase 
+
+def zernike_no_aperture(amplitude = 1, radial=0,azimuthal=0,  x0=None,y0=None,radius=None,shape=(512,512)):
+
+    if azimuthal > radial:
+        raise ValueError("azimuthal must be less than or equal to radial")
+    if x0 is None:
+        x0 = shape[0]/2
+    if y0 is None:
+        y0 = shape[1]/2
+    if radius is None:
+        radius = min([shape[0]-x0,shape[1]-y0,x0,y0])
+    x = np.arange(shape[0])
+    y = np.arange(shape[1])
+    xx,yy = np.meshgrid(x,y)
+    r = np.sqrt((xx-x0)**2+(yy-y0)**2)
+    r /= radius
+    phi = np.arctan2(yy-y0,xx-x0)
+    if azimuthal >= 0:
+        phase = R(r,radial,azimuthal)*np.cos(azimuthal*phi)
+    else:
+        phase = R(r,radial,-azimuthal)*np.sin(-azimuthal*phi)
+    phase *= amplitude
+
+    return phase 
+
+def sum_zernike(coeffs):
+    total_sum = 0
+
+    for coeff, zernike in zip(coeffs, CACHED_ZERNIKE):
+        total_sum += coeff * zernike
     
-    return hologram
+    return total_sum
+    
 
-hologram = zernike(radial=0,azimuthal=0,amplitude=1,x0=None,y0=None,radius=None,shape=(512,512))
+def zernike_decomposition(trap_hologram,  x0=None, y0=None, radius=None, shape=(512,512)):
 
-print(zernike_decomposition(hologram, 0, 0, 0.5, 
-                        circ_aperture = True, 
-                        x0 = None , 
-                        y0 = None , 
-                        radius = None, 
-                        shape = (512, 512)))
+    def objective_func(x):
+        return np.sum((trap_hologram - sum_zernike(x))**2)
 
+    amplitudes = minimize(objective_func, x0=np.zeros(len(ZERNIKE_COORDINATES)), bounds=[(-1, 1) for i in range(len(ZERNIKE_COORDINATES))])
+
+    return amplitudes
+
+ZERNIKE_COORDINATES = (
+    # radial, azimuthal
+    (0,0),
+    (1,1),
+    (1,-1),
+    (2,2),
+    (2,0),
+    (2,-2),
+    (3,3),
+    (3,1),
+    (3,-1),
+    (3,-3),
+    (4,4),
+    (4,2),
+    (4,0),
+    (4,-2),
+    (4,-4)
+)
+
+CACHED_ZERNIKE = [zernike_no_aperture_no_amp(rad, azi) for rad,azi in ZERNIKE_COORDINATES]
+
+trap_hologram = zernike_no_aperture_no_amp(4,4)
+
+
+print(zernike_decomposition(trap_hologram))
+
+final = 0
+for i in range(len(amplitudes)):
+    rad, azi = ZERNIKE_COORDINATES[i]
+    final = final + zernike_no_aperture(amplitudes[i], rad, azi)
+
+
+plt.pcolor(final)
+plt.show()
